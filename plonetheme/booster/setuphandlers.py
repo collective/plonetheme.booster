ecc0b7485e84c026e1c253231025b259d1f8f8b8
vikas-parashar <svnitvikas@gmail.com>
2016-07-17 00:46:57 +0530

    carousel done. Yay

--- plonetheme/booster/setuphandlers.py
+++ plonetheme/booster/setuphandlers.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
-
+from plone import api
+import os
 
 @implementer(INonInstallable)
 class HiddenProfiles(object):
@@ -15,9 +16,49 @@ class HiddenProfiles(object):
 
 def post_install(context):
     """Post install script"""
-    # Do something at the end of the installation of this package.
+    if context.readDataFile('plonethemetango_default.txt') is None:
+        return
+    # Do something during the installation of this package
+    portal = api.portal.get()
+    _create_content(portal)
+
+
+def _create_content(portal):
+    if not portal.get('slider-images', False):
+        slider = api.content.create(
+            type='Folder',
+            container=portal,
+            title=u'Slider',
+            id='slider-images'
+        )
+        for slider_number in range(1, 4):
+            slider_name = u'slider-{0}'.format(str(slider_number))
+            slider_image = api.content.create(
+                type='Image',
+                container=slider,
+                id=slider_name
+            )
+            slider_image.image = load_image(slider_number)
+        # NOTE: if your plone site is not a vanilla plone
+        # you can have different workflows on folders and images
+        # or different transitions names so this could fail
+        # and you'll need to publish the images as well
+        # or do that manually TTW.
+        api.content.transition(obj=slider, transition='publish')
+
+
+def load_image(slider):
+    from plone.namedfile.file import NamedBlobImage
+    filename = os.path.join(os.path.dirname(__file__), 'theme', 'img',
+                            'slide-{0}.jpg'.format(slider))
+    return NamedBlobImage(
+        data=open(filename, 'r').read(),
+        filename=u'slide-{0}.jpg'.format(slider)
+    )
 
 
 def uninstall(context):
     """Uninstall script"""
-    # Do something at the end of the uninstallation of this package.
+    if context.readDataFile('plonethemetango_uninstall.txt') is None:
+        return
+        # Do something during the uninstallation of this package
--- plonetheme/booster/theme/scripts/main.js
+++ plonetheme/booster/theme/scripts/main.js
@@ -26,71 +26,6 @@
 		});
 	};
 
-	// var owlCrouselFeatureSlide = function() {
-
-	// 	var owl = $('.owl-carousel');
-
-	// 	owl.on('initialized.owl.carousel change.owl.carousel',function(elem){
-	// 		var current = elem.item.index;
-	// 		$(elem.target).find('.owl-item').eq(current).find('.to-animate').removeClass('fadeInUp animated');
-	// 	});
-	// 	owl.on('initialized.owl.carousel changed.owl.carousel',function(elem){
-	// 		window.setTimeout(function(){
-	// 			var current = elem.item.index;
-	// 			$(elem.target).find('.owl-item').eq(current).find('.to-animate').addClass('fadeInUp animated');
-	// 		}, 400);
- //     	});
-
-	// 	owl.owlCarousel({
-	// 		items: 1,
-	// 	    loop: true,
-	// 	    margin: 0,
-	// 	    responsiveClass: true,
-	// 	    nav: true,
-	// 	    dots: true,
-	// 	    smartSpeed: 500,
-	// 	    autoplay: true,
-	// 		autoplayTimeout: 5000,
-	// 		autoplayHoverPause: true,
-	// 	    navText: [
-	// 	      '<i class=\'icon-arrow-left22 owl-direction\'></i>',
-	// 	      '<i class=\'icon-arrow-right22 owl-direction\'></i>'
-	//      	],
-
-	// 	});
-
-	// };
-
-
-	// Magnific Popup
-	// MagnificPopup
-	var magnifPopup = function() {
-		$('.image-popup').magnificPopup({
-			type: 'image',
-			removalDelay: 300,
-			mainClass: 'mfp-with-zoom',
-			gallery:{
-				enabled:true
-			},
-			zoom: {
-				enabled: true, // By default it's false, so don't forget to enable it
-
-				duration: 300, // duration of the effect, in milliseconds
-				easing: 'ease-in-out', // CSS transition easing function
-
-				// The "opener" function should return the element from which popup will be zoomed in
-				// and to which popup will be scaled down
-				// By defailt it looks for an image tag:
-				opener: function(openerElement) {
-				// openerElement is the element on which popup was initialized, in this case its <a> tag
-				// you don't need to add "opener" option if this code matches your needs, it's defailt one.
-				return openerElement.is('img') ? openerElement : openerElement.find('img');
-				}
-			}
-		});
-	};
-
-
 	// Animate Feature
 	var animateFeatureIcons = function() {
 		if ( $('#fh5co-features').length > 0 ) {
@@ -200,9 +135,6 @@
 	$(function(){
 
 		burgerMenu();
-		// owlCrouselFeatureSlide();
-		// magnifPopup();
-
 		featureIconsWayPoint();
 		productsWayPoint();
 		clientsWayPoint();
@@ -218,6 +150,14 @@
 	$('#portal-searchbox .LSBox').hover(function(){
 	  $('.searchSection').toggleClass('show-searchSection');
 	});
+
+	$('.item.active').find(".to-animate").addClass('fadeInUp animated');
+	$('#carousel-example-generic').on('slid.bs.carousel', function () {
+		$('.item.active').find(".to-animate").addClass('fadeInUp animated');
+	})
+	$('#carousel-example-generic').on('slide.bs.carousel', function () {
+		$('.item.active').find(".to-animate").removeClass('fadeInUp animated');
+	})
 
 
 }());
