0b40bf7eda057c2d2464f043d92371bce261155f
vikas-parashar <svnitvikas@gmail.com>
2016-07-23 19:39:06 +0530

    setup.py added .pyc files removed

---
+++ setup.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""Installer for the plonetheme.booster package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+long_description = '\n\n'.join([
+    open('README.rst').read(),
+    open('CHANGES.rst').read(),
+])
+
+
+setup(
+    name='plonetheme.booster',
+    version='1.0',
+    description="A theme for Plone 5",
+    long_description=long_description,
+    # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords='Python Plone Theme <web></web>',
+    author='Vikas Parashar',
+    author_email='svnitvikas@gmail.com',
+    url='https://pypi.python.org/pypi/plonetheme.booster',
+    license='MIT License',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plonetheme'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'plone.api',
+        'Products.GenericSetup>=1.8.2',
+        'setuptools',
+        'z3c.jbot',
+        'plone.app.theming',
+        'plone.app.themingplugins',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            # Plone KGS does not use this version, because it would break
+            # Remove if your package shall be part of coredev.
+            # plone_coredev tests as of 2016-04-01.
+            'plone.testing>=5.0.0',
+            'plone.app.contenttypes',
+            'plone.app.robotframework[debug]',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
