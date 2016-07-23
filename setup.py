ec814bb9a9c37c0b396416bea5fdf342979d01f6
vikas-parashar <svnitvikas@gmail.com>
2016-07-23 23:13:35 +0530

    Preparing release 1.0.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='plonetheme.booster',
-    version='1.0',
+    version='1.0.1',
     description="A theme for Plone 5",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
