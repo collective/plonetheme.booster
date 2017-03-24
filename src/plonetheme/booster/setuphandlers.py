335e8ba489f15d07a2755627a639fe6b30f086ed
Alec Mitchell <alecpm@gmail.com>
2017-03-23 17:04:44 -0700

    Use enable-bundles feature of plone.app.theming. Don't leave site unthemed on uninstall.

--- src/plonetheme/booster/setuphandlers.py
+++ src/plonetheme/booster/setuphandlers.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 from Products.CMFPlone.interfaces import INonInstallable
+from zope.component import getUtility
 from zope.interface import implementer
 from plone import api
+from plone.app.theming.interfaces import IThemeSettings
+from plone.app.theming.utils import applyTheme
+from plone.app.theming.utils import getAvailableThemes
+from plone.app.theming.utils import getCurrentTheme
+from plone.registry.interfaces import IRegistry
 import os
+
+DEFAULT_THEME = u'barceloneta'
 
 
 @implementer(INonInstallable)
@@ -14,6 +22,7 @@ class HiddenProfiles(object):
             'plonetheme.booster:uninstall',
         ]
 
+
 def post_install(context):
     """Post install script"""
     # Do something at the end of the installation of this package.
@@ -21,4 +30,17 @@ def post_install(context):
 
 def uninstall(context):
     """Uninstall script"""
-    # Do something at the end of the uninstallation of this package.
+    if hasattr(context, 'getSite'):
+        if context.readDataFile('plonethemebooster_uninstall.txt') is None:
+            return
+
+    registry = getUtility(IRegistry)
+    theme_settings = registry.forInterface(IThemeSettings, False)
+    current_theme = getCurrentTheme()
+    if not current_theme or current_theme == u'plonetheme.booster':
+        themes = getAvailableThemes()
+        for theme in themes:
+            if theme.__name__ == DEFAULT_THEME:
+                applyTheme(theme)
+                theme_settings.enabled = True
+                break
